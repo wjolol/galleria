9fcc900effa82fe8646e2bc8ad816093d2be1e42
Giorgio Sertori <giorgio@MacBook-Pro-di-Giorgio.local>
2018-09-21 12:54:49 +0200

    first commit

---
+++ galleria/AuthenticationViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AuthenticationViewController.swift
+//  galleria
+//
+//  Created by Giorgio Sertori on 20/09/18.
+//  Copyright © 2018 Giorgio Sertori. All rights reserved.
+//
+
+import Foundation
